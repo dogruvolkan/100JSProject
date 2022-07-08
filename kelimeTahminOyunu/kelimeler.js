39dc94776ccb4354c780736c2da41ed163aede54
volkan <volkandogru11@gmail.com>
2022-07-08 20:18:23 +0300

    kelime bulma fonksiyonu eklendi

--- kelimeTahminOyunu/kelimeler.js
+++ kelimeTahminOyunu/kelimeler.js
@@ -1,26 +1,50 @@
-const kelimeler=[
+const kelimeler = [
     {
-        kelime:"python",
-        ipUcu : "Programlama dili"
+        kelime: "python",
+        ipUcu: "Programlama dili"
     },
     {
-        kelime:"kedi",
-        ipUcu : "Bir hayvan"
+        kelime: "kedi",
+        ipUcu: "Bir hayvan"
     },
     {
-        kelime:"etek",
-        ipUcu : "Kadın giysisi"
+        kelime: "etek",
+        ipUcu: "Kadın giysisi"
     },
     {
-        kelime:"kalem",
-        ipUcu : "Kırtasiye malzemesi"
+        kelime: "kalem",
+        ipUcu: "Kırtasiye malzemesi"
     },
     {
-        kelime:"araba",
-        ipUcu : "Kara ulaşım aracı"
+        kelime: "araba",
+        ipUcu: "Kara ulaşım aracı"
     },
     {
-        kelime:"televizyon",
-        ipUcu : "aptal kutu"
+        kelime: "televizyon",
+        ipUcu: "aptal kutu"
+    },
+    {
+        kelime: "karpuz",
+        ipUcu: "bir yaz meyvesi"
+    },
+    {
+        kelime: "kumru",
+        ipUcu: "Bir kuş "
+    },
+    {
+        kelime: "heap",
+        ipUcu: "Diziler belleğin hangi bölgesinde tutulur"
+    },
+    {
+        kelime: "antalya",
+        ipUcu: "Türkiyenin güneyinde bir il"
+    },
+    {
+        kelime: "volkan",
+        ipUcu: "javascripti en çok seven yazılımcı"
+    },
+    {
+        kelime: "gigabyte",
+        ipUcu: "1024 megabyte 1 nedir"
     }
 ]
--- kelimeTahminOyunu/script.js
+++ kelimeTahminOyunu/script.js
@@ -1,61 +1,83 @@
-const inputs = document.querySelector(".inputs");
+const inputsDom = document.querySelector(".inputs");
 const resetBtn = document.querySelector(".resetBtn");
 const ipUcuDom = document.querySelector(".ipUcu span");
 const kalanHakDom = document.querySelector(".kalanTahmin span");
 const typingDom = document.querySelector(".typing");
 const yanlisHarfDom = document.querySelector(".yanlisHarf span");
+let skorDogruDom = document.querySelector(".skor1 span");
+let skorYanlisDom = document.querySelector(".skor2 span");
 
 let kelime;
 let dogruHarfDizisi = [];
 let yanlisHarfDizisi = [];
 let tahminHakki;
+let sayacDogru = 0;
+let sayacYanlis = 0;
 
-function randomWord() {
-    let randObj = kelimeler[Math.floor(Math.random() * kelimeler.length)];
-    kelime = randObj.kelime;
-    let ipUcu = randObj.ipUcu;
+function rastgeleKelime() {
+    let rastgeleSayi = kelimeler[Math.floor(Math.random() * kelimeler.length)];
+    kelime = rastgeleSayi.kelime;
+    let ipUcu = rastgeleSayi.ipUcu;
     tahminHakki = 8;
+    dogruHarfDizisi = [];
+    yanlisHarfDizisi = [];
 
     ipUcuDom.innerHTML = ipUcu;
     kalanHakDom.innerHTML = tahminHakki;
+    yanlisHarfDom.innerHTML = yanlisHarfDizisi;
 
-    let html = "";
+    let htmlTaslak = "";
     for (let i = 0; i < kelime.length; i++) {
-        html += '<input type="text" disabled>'
+        htmlTaslak += '<input type="text" disabled>'
     }
-    inputs.innerHTML = html;
-
-
+    inputsDom.innerHTML = htmlTaslak;
 }
 
-function initGame(e) {
+
+
+function oyunuBaslatma(e) {
     let key = e.target.value;
     if (key.match(/^[A-Za-z]+$/) && !yanlisHarfDizisi.includes(` ${key}`) && !dogruHarfDizisi.includes(key)) {
-        console.log(key);
         yanlisHarfDom.innerHTML = "";
         if (kelime.includes(key)) {
             for (let i = 0; i < kelime.length; i++) {
                 if (kelime[i] === key) {
                     dogruHarfDizisi.push(key);
-                    inputs.querySelectorAll("input")[i].value = key;
+                    inputsDom.querySelectorAll("input")[i].value = key;
                 }
             }
         }
         else {
             tahminHakki--;
-
             yanlisHarfDizisi.push(` ${key}`);
-
         }
         kalanHakDom.innerHTML = tahminHakki;
         yanlisHarfDom.innerHTML = yanlisHarfDizisi;
+
     }
-
-
     typingDom.value = "";
+
+
+    setTimeout(() => {
+        if (dogruHarfDizisi.length === kelime.length) {
+            alert(`Kazandın ${kelime.toUpperCase()}`);
+            sayacDogru++;
+            skorDogruDom.innerHTML = sayacDogru;
+            rastgeleKelime();
+        }
+        else if (tahminHakki < 1) {
+            alert(`Kaybettin ${kelime.toUpperCase()}`)
+            sayacYanlis++;
+            skorYanlisDom.innerHTML = sayacYanlis;
+            for (let i = 0; i < kelime.length; i++) {
+                inputsDom.querySelectorAll("input")[i].value = kelime[i];
+            }
+        }
+    }, 300);
 }
 
 
-resetBtn.addEventListener("click", randomWord);
-typingDom.addEventListener("input", initGame);
+resetBtn.addEventListener("click", rastgeleKelime);
+typingDom.addEventListener("input", oyunuBaslatma);
+inputsDom.addEventListener("click", () => typingDom.focus());
 document.addEventListener("keydown", () => typingDom.focus());
